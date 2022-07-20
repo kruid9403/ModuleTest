742859ad84a9ed27d20f30f197b655c12a6d5cbd
Jeremy <jeremy.kruid@jedmahonisgroup.com>
2022-07-20 17:02:31 -0500

    a message 3

--- TestModule/src/main/java/com/jeremykruid/testmodule/TextTest.kt
+++ TestModule/src/main/java/com/jeremykruid/testmodule/TextTest.kt
@@ -4,20 +4,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.google.firebase.auth.FirebaseAuth
 
-class TestClass() {
-    @Composable
-    fun TextTest() {
-        Text(text = "some text that might work")
-    }
+@Composable
+fun TextTest(){
+    Text(text = "some text that might work")
+}
 
-    fun getUId(): String {
-        return FirebaseAuth.getInstance().currentUser.toString()
-    }
+fun getUId(): String{
+    return FirebaseAuth.getInstance().currentUser.toString()
+}
 
-    fun loginUser(loggedIn: () -> Unit) {
-        FirebaseAuth.getInstance().signInWithEmailAndPassword("a@a.com", "111111")
-            .addOnCompleteListener {
-                loggedIn.invoke()
-            }
-    }
+fun loginUser(loggedIn: () -> Unit){
+    FirebaseAuth.getInstance().signInWithEmailAndPassword("a@a.com", "111111")
+        .addOnCompleteListener {
+            loggedIn.invoke()
+        }
 }
--- app/src/main/java/com/jeremykruid/moduletest/MainActivity.kt
+++ app/src/main/java/com/jeremykruid/moduletest/MainActivity.kt
@@ -8,19 +8,27 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import com.jeremykruid.moduletest.ui.theme.ModuleTestTheme
-import com.jeremykruid.testmodule.TextTest
+import com.jeremykruid.testmodule.TestClass
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val text = remember{ mutableStateOf("")}
         setContent {
             ModuleTestTheme {
                 // A surface container using the 'background' color from the theme
                 Column() {
                     Greeting("Android")
-                    TextTest()
+//                    TestClass().TextTest()
+//                    TestClass().loginUser {
+//                        text.value = TestClass().getUId()
+//                    }
+//
+//                    Text(text = text.value)
                 }
             }
         }
