09290f15b99c34c3fa087ad374cc74693267d29c
Jeremy <jeremy.kruid@jedmahonisgroup.com>
2022-07-20 16:23:29 -0500

    a message

--- TestModule/src/main/java/com/jeremykruid/testmodule/TextTest.kt
+++ TestModule/src/main/java/com/jeremykruid/testmodule/TextTest.kt
@@ -2,8 +2,20 @@ package com.jeremykruid.testmodule
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun TextTest(){
     Text(text = "some text that might work")
+}
+
+fun getUId(): String{
+    return FirebaseAuth.getInstance().currentUser.toString()
+}
+
+fun loginUser(loggedIn: () -> Unit){
+    FirebaseAuth.getInstance().signInWithEmailAndPassword("a@a.com", "111111")
+        .addOnCompleteListener {
+            loggedIn.invoke()
+        }
 }
