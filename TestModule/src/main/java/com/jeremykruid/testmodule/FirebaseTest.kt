badb3391b4c3d095dee00dc4fb6441451836fdb9
Jeremy <jeremy.kruid@jedmahonisgroup.com>
2022-07-21 16:06:02 -0500

    a message 7

--- TestModule/src/main/java/com/jeremykruid/testmodule/FirebaseTest.kt
+++ TestModule/src/main/java/com/jeremykruid/testmodule/FirebaseTest.kt
@@ -1,34 +1,37 @@
 package com.jeremykruid.testmodule
 
-import android.app.Application
 import android.content.Context
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.AndroidViewModel
 import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuth.AuthStateListener
+import com.google.firebase.auth.FirebaseAuth.getInstance
 import com.google.firebase.auth.FirebaseUser
 
-class FirebaseTest(application: Application): AndroidViewModel(application) {
+class FirebaseTest {
 
     var instance: FirebaseAuth? = null
     val currentUser = mutableStateOf<FirebaseUser?>(null)
 
-    fun init(auth: FirebaseAuth, context: Context){
-        FirebaseApp.initializeApp(getApplication())
+    fun init(auth: FirebaseAuth){
         instance = auth
-        setAuthListener()
     }
 
-    fun setAuthListener(){
+    fun setAuthListener(authChanged: (FirebaseUser?)-> Unit){
         instance?.addAuthStateListener(object : AuthStateListener{
             override fun onAuthStateChanged(p0: FirebaseAuth) {
                 currentUser.value = p0.currentUser
+                authChanged.invoke(p0.currentUser)
             }
         })
     }
 
     fun signInWithEmail(){
-        instance?.signInWithEmailAndPassword("a@a.com", "111111")
+        instance?.signInWithEmailAndPassword("b@a.com", "111111")
+    }
+
+    fun signOut(){
+        instance?.signOut()
     }
 }
--- app/src/main/java/com/jeremykruid/moduletest/MainActivity.kt
+++ app/src/main/java/com/jeremykruid/moduletest/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.jeremykruid.moduletest
 
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.jeremykruid.moduletest.ui.theme.ModuleTestTheme
 import com.jeremykruid.testmodule.FirebaseTest
@@ -17,11 +22,16 @@ import com.jeremykruid.testmodule.TextTest
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FirebaseApp.initializeApp(this)
+
+        val options = FirebaseOptions.Builder()
+        options.setApiKey("AIzaSyCd6Oy6xIq_BmAzGN1J7iPb57iug0Q5h-A")
+        options.setApplicationId("1:277277686872:android:1faeb3a3ed990722279e2d")
+        val firebaseTest = FirebaseTest()
+        FirebaseApp.initializeApp(this, options.build())
         val auth = FirebaseAuth.getInstance()
-        val firebaseTest = FirebaseTest(application)
         setContent {
             ModuleTestTheme {
+                val firstRun = remember { mutableStateOf(true)}
                 val context = LocalContext.current
 //                FirebaseApp.initializeApp(context)
                 // A surface container using the 'background' color from the theme
@@ -33,11 +43,22 @@ class MainActivity : ComponentActivity() {
 //                    }
 //
 //                    Text(text = text.value
-                    firebaseTest.init(auth = auth, context = context)
-                    firebaseTest.signInWithEmail()
+                    firebaseTest.init(auth = auth)
+                    firebaseTest.setAuthListener(
+                        authChanged = {
+                            Toast.makeText(context, it?.uid.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                    if (firstRun.value) {
+                        firebaseTest.signInWithEmail()
+                        firstRun.value = false
+                    }
                     if(firebaseTest.currentUser.value != null) {
                         Text(text = firebaseTest.instance?.currentUser?.email.toString())
                     }
+                    Handler().postDelayed({
+                        firebaseTest.signOut()
+                    },5000)
                 }
             }
         }
